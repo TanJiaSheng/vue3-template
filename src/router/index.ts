508cb7835f377e73ce92d5363c88bbc3e6906016
Ten <tentan@canway.net>
2024-03-04 15:21:59 +0800

    feature: 引入router@4

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes: constantRouter,
     // 滚动行为
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+    scrollBehavior: () => ({ left: 0, top: 0 })
 })
 
 export default router
